9ace963349594fa6ab467463a460f92ea27f46b5
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-27 09:41:08 -0600

    Fix lint issues

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import "source-map-support/register.js";
-import { version } from "./version";
+import 'source-map-support/register.js';
+import { version } from './version';
 
 // TODO: Start the client, handle events, dispatch out to handler functions, etc.
 
-console.debug(`Hello, world! Version ${version} has run.`);
+console.debug(`Hello, world! CS Bot v${version}, at your service.`);
