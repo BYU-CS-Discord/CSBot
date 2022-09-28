3ebd3ca13975703d4206f70c9fa60be6766591de
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-28 08:26:09 -0600

    Remove useless isDiscordError guard

--- src/helpers/guards/isDiscordError.ts
+++
@@ -1,8 +0,0 @@
-import { DiscordAPIError } from 'discord.js';
-
-/**
- * Asserts that a value is a Discord API error.
- */
-export function isDiscordError(tbd: unknown): tbd is DiscordAPIError {
-	return tbd instanceof DiscordAPIError;
-}
