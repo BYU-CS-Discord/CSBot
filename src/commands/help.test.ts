5c38aece3638804ddfa806dde5476c576c48440e
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-28 23:55:29 -0600

    Add tests around guild-only commands

--- src/commands/help.test.ts
+++ src/commands/help.test.ts
@@ -1,11 +1,21 @@
 import type { EmbedBuilder } from 'discord.js';
 import { help } from './help';
 
+const { allCommands: realAllCommands } = jest.requireActual<typeof import('./index')>('./index');
+const mockAllCommands = new Map<string, Command>();
+
+jest.mock('./index', () => ({
+	allCommands: mockAllCommands,
+}));
+
 describe('help', () => {
 	const mockReply = jest.fn();
 	let context: CommandContext;
 
 	beforeEach(() => {
+		mockAllCommands.clear();
+		realAllCommands.forEach((value, key) => mockAllCommands.set(key, value));
+
 		context = {
 			guild: null,
 			reply: mockReply,
@@ -23,6 +33,7 @@ describe('help', () => {
 		});
 		const call = mockReply.mock.calls[0] as [{ embeds: [EmbedBuilder] }];
 		const embed = call[0].embeds[0];
+		expect(embed.data.fields).toBeArrayOfSize(mockAllCommands.size);
 		expect(embed.data).toMatchSnapshot();
 	});
 
@@ -40,6 +51,30 @@ describe('help', () => {
 		});
 		const call = mockReply.mock.calls[0] as [{ embeds: [EmbedBuilder] }];
 		const embed = call[0].embeds[0];
+		expect(embed.data.fields).toBeArrayOfSize(mockAllCommands.size);
 		expect(embed.data).toMatchSnapshot();
+	});
+
+	test("doesn't show a guild-only command when in DMs", async () => {
+		const cmd: GuildedCommand = {
+			name: 'canttouchthis-do-do-do-do', // this test will break if we ever have a command with this name
+			description: "Can't touch this. (This is a test.)",
+			requiresGuild: true,
+			dmPermission: false,
+			execute() {
+				// nop
+			},
+		};
+		mockAllCommands.set(cmd.name, cmd);
+
+		await expect(help.execute(context)).resolves.toBeUndefined();
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			embeds: [expect.toBeObject()],
+			ephemeral: true,
+		});
+		const call = mockReply.mock.calls[0] as [{ embeds: [EmbedBuilder] }];
+		const embed = call[0].embeds[0];
+		expect(embed.data.fields).toBeArrayOfSize(mockAllCommands.size - 1);
 	});
 });
