8c357efeb07530dc255eb0832fc197c0425fa6d8
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-28 23:05:30 -0600

    Update unit tests

--- src/commands/help.test.ts
+++ src/commands/help.test.ts
@@ -1,15 +1,45 @@
+import type { EmbedBuilder } from 'discord.js';
 import { help } from './help';
 
 describe('help', () => {
-	test("it's just a Hello World", async () => {
-		const mockReply = jest.fn();
-		const context = {
+	const mockReply = jest.fn();
+	let context: CommandContext;
+
+	beforeEach(() => {
+		context = {
+			guild: null,
 			reply: mockReply,
+		} as unknown as CommandContext;
+	});
+
+	test('presents an ephemeral embed with all available global commands', async () => {
+		context = { ...context, guild: null };
+
+		await expect(help.execute(context)).resolves.toBeUndefined();
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			embeds: [expect.toBeObject()],
+			ephemeral: true,
+		});
+		const call = mockReply.mock.calls[0] as [{ embeds: [EmbedBuilder] }];
+		const embed = call[0].embeds[0];
+		expect(embed.data).toMatchSnapshot();
+	});
+
+	test('presents an ephemeral embed with all available global and guild-bound commands', async () => {
+		context = {
+			...context,
+			guild: { id: 'the-guild-1234' },
 		} as unknown as CommandContext;
 
 		await expect(help.execute(context)).resolves.toBeUndefined();
-
 		expect(mockReply).toHaveBeenCalledOnce();
-		expect(mockReply).toHaveBeenCalledWith('Hello, world!');
+		expect(mockReply).toHaveBeenCalledWith({
+			embeds: [expect.toBeObject()],
+			ephemeral: true,
+		});
+		const call = mockReply.mock.calls[0] as [{ embeds: [EmbedBuilder] }];
+		const embed = call[0].embeds[0];
+		expect(embed.data).toMatchSnapshot();
 	});
 });
