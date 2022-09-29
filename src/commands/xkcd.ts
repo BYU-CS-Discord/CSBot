90ab518ecf74f32102d34e488e62b2bb9099d43b
gmacgre <gmacgre@byu.edu>
2022-09-29 01:22:34 -0600

    Added basic xkcd comand side functionality

--- src/commands/xkcd.ts
+++ src/commands/xkcd.ts
@@ -1,9 +1,24 @@
+import { ApplicationCommandOptionType } from 'discord.js';
+
 export const xkcd: GlobalCommand = {
 	name: 'xkcd',
 	description: 'Fetches the most recent xkcd comic, or a selected ',
 	requiresGuild: false,
-	async execute({ reply }) {
+	options: [
+		{
+			name: 'number',
+			description: 'The index number of the comic you would like to have',
+			type: ApplicationCommandOptionType.Integer,
+		},
+	],
+	async execute({ options, reply, sendTyping }) {
 		// TODO: finish this command
-		await reply('Hello, world!');
+		sendTyping;
+		const param = options[0];
+		if (param?.value !== undefined) {
+			await reply(`You sent a param! ${param.value as number}`);
+		} else {
+			await reply('You did not send a param.');
+		}
 	},
 };
