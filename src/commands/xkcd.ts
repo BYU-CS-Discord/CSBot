6b1ba1a3a28de8b20df553dad5397a486071ef43
gmacgre <gmacgre@byu.edu>
2022-09-28 23:24:01 -0600

    Added base files for the command

---
+++ src/commands/xkcd.ts
@@ -0,0 +1,9 @@
+export const xkcd: GlobalCommand = {
+	name: 'xkcd',
+	description: 'Fetches the most recent xkcd comic, or a selected ',
+	requiresGuild: false,
+	async execute({ reply }) {
+		// TODO: finish this command
+		await reply('Hello, world!');
+	},
+};
