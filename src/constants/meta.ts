8f121528c726909e5ed8ac5baec2ec3b90e7fb33
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-30 10:21:05 -0600

    Store important metadata constants somewhere reasonable

---
+++ src/constants/meta.ts
@@ -0,0 +1,9 @@
+import { URL } from 'node:url';
+
+/**
+ * A link to the bot's public git repository.
+ */
+export const repo = new URL('https://github.com/BYU-CS-Discord/CSBot');
+
+// If TS throws errors here, run `npm run lint` or `npm run export-version`
+export { version as appVersion } from '../version';
