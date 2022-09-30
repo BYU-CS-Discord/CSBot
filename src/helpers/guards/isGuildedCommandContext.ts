2bb19fd3aac8c526f7e6c5f2074285d0e52bb8f4
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-30 12:22:55 -0600

    Remove deprecated structures

--- src/helpers/guards/isGuildedCommandContext.ts
+++
@@ -1,4 +0,0 @@
-/** @deprecated Check `tbd.source === 'guild'` instead. */
-export function isGuildedCommandContext(tbd: CommandContext): tbd is GuildedCommandContext {
-	return tbd.source === 'guild';
-}
