1328839a24ad11a2aac218128b02a9e8e3b0d56f
gmacgre <gmacgre@byu.edu>
2022-10-01 17:52:58 -0600

    Final changes

--- src/commands/xkcd.test.ts
+++ src/commands/xkcd.test.ts
@@ -1,4 +1,50 @@
+import axios from 'axios';
 import { xkcd } from './xkcd';
+jest.mock('axios');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const latestGood = {
+	status: 200,
+	data: {
+		month: '9',
+		num: 2679,
+		link: '',
+		year: '2022',
+		news: '',
+		safe_title: 'Quantified Self',
+		transcript: '',
+		alt: "It's made me way more excited about ferris wheels, subways, car washes, waterslides, and store entrances that have double doors with a divider in the middle.",
+		img: 'https://imgs.xkcd.com/comics/quantified_self.png',
+		title: 'Quantified Self',
+		day: '30',
+	},
+};
+
+const chosen = {
+	month: '1',
+	num: 35,
+	link: '',
+	news: '',
+	safe_title: 'Sheep',
+	transcript:
+		"Heading: Another from my high-school notebooks.\n[[A sheep and a potted saguaro cactus linked by an arcing yellow electricity bolt, drawn on graph paper]]\n{{title text: I think it's the sheep zapping the cactus and not vice-versa}}",
+	alt: "I think it's the sheep zapping the cactus and not vice-versa",
+	year: '2006',
+	title: 'Sheep',
+	day: '1',
+	img: 'https://imgs.xkcd.com/comics/sheep.jpg',
+};
+
+const chosenGood = {
+	status: 200,
+	data: chosen,
+};
+
+const badResponse = {
+	status: 400,
+	data: null,
+};
 
 describe('xkcd', () => {
 	const mockReply = jest.fn();
@@ -13,9 +59,13 @@ describe('xkcd', () => {
 	});
 
 	test('Returns an ephemeral error message when the number is out of bounds', async () => {
+		mockedAxios.get.mockResolvedValue(latestGood);
+		// they just need the number from the initial call
+
 		context = { ...context, options: [{ value: -1 }] } as unknown as CommandContext;
 		await expect(xkcd.execute(context)).resolves.toBeUndefined();
 		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockSendTyping).toHaveBeenCalledOnce();
 		expect(mockReply).toHaveBeenCalledWith({
 			content: 'Please insert a valid comic number. The range is 1-2679.',
 			ephemeral: true,
@@ -25,6 +75,7 @@ describe('xkcd', () => {
 
 		await expect(xkcd.execute(context)).resolves.toBeUndefined();
 		expect(mockReply).toHaveBeenCalledTimes(2);
+		expect(mockSendTyping).toHaveBeenCalledTimes(2);
 		expect(mockReply).toHaveBeenCalledWith({
 			content: 'Please insert a valid comic number. The range is 1-2679.',
 			ephemeral: true,
@@ -32,12 +83,52 @@ describe('xkcd', () => {
 	});
 
 	test('Returning an embed with the latest comic when no number is given', async () => {
+		mockedAxios.get.mockResolvedValue(latestGood);
 		context = { ...context, options: [] } as unknown as CommandContext;
 		await expect(xkcd.execute(context)).resolves.toBeUndefined();
 		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockSendTyping).toHaveBeenCalledOnce();
 		expect(mockReply).toHaveBeenCalledWith({
 			embeds: [expect.toBeObject()],
 			ephemeral: false,
+		});
+	});
+
+	test('Returning an embed with a comic given by a number parameter', async () => {
+		mockedAxios.get.mockResolvedValue(chosenGood);
+		mockedAxios.get.mockResolvedValueOnce(latestGood);
+		context = { ...context, options: [{ value: 35 }] } as unknown as CommandContext;
+		await expect(xkcd.execute(context)).resolves.toBeUndefined();
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockSendTyping).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			embeds: [expect.toBeObject()],
+			ephemeral: false,
+		});
+	});
+
+	test('Checking when a second call to the API fails', async () => {
+		mockedAxios.get.mockResolvedValue(badResponse);
+		mockedAxios.get.mockResolvedValueOnce(latestGood);
+		context = { ...context, options: [{ value: 35 }] } as unknown as CommandContext;
+		await expect(xkcd.execute(context)).resolves.toBeUndefined();
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockSendTyping).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			content: 'XKCD call returned an error. Please try again later.',
+			ephemeral: true,
+		});
+	});
+
+	test('Checking when a first call to the API fails', async () => {
+		mockedAxios.get.mockResolvedValue(badResponse);
+		context = { ...context, options: [{ value: 35 }] } as unknown as CommandContext;
+		await expect(xkcd.execute(context)).resolves.toBeUndefined();
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockSendTyping).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			content: 'XKCD call failed. Please try again later.',
+			ephemeral: true,
 		});
 	});
 });
--- src/commands/xkcd.ts
+++ src/commands/xkcd.ts
@@ -1,4 +1,9 @@
-import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import {
+	ApplicationCommandOptionType,
+	EmbedBuilder,
+	InteractionReplyOptions,
+	MessageReplyOptions,
+} from 'discord.js';
 import axios from 'axios';
 
 // defining the response
@@ -16,9 +21,49 @@ interface GetComicResponse {
 	img: string;
 }
 
+// a quick call to see the latest comic.
+// @return the data of the most recent comic, or an empty response with num set to -1 to indicate an error.
 async function _latestCheck(): Promise<GetComicResponse> {
-	const { data } = await axios.get<GetComicResponse>('https://xkcd.now.sh/?comic=latest');
-	return data;
+	try {
+		const { data, status } = await axios.get<GetComicResponse>('https://xkcd.now.sh/?comic=latest');
+		if (status !== 200) throw new Error('No good status');
+		return data;
+	} catch (error_) {
+		console.log(error_);
+		const error: GetComicResponse = {
+			month: '',
+			num: -1,
+			link: '',
+			news: '',
+			safe_title: '',
+			transcript: '',
+			alt: '',
+			year: '',
+			title: '',
+			day: '',
+			img: '',
+		};
+		return error;
+	}
+}
+
+// this is kinda cursed, but its the best way that I can pass "reply" as a parameter to minimize
+// code duplication
+// sends an ephemeral message to the user
+// @param reply: the reply method given in the execute method's CommandContext
+// @param content: the string that should be sent to the client to let them know the error.
+async function _sendErr(
+	reply: (
+		options:
+			| string
+			| Omit<MessageReplyOptions, 'flags'>
+			| (Omit<InteractionReplyOptions, 'flags'> & {
+					shouldMention?: boolean;
+			  })
+	) => Promise<void>,
+	content: string
+): Promise<void> {
+	await reply({ content: content, ephemeral: true });
 }
 
 export const xkcd: GlobalCommand = {
@@ -50,19 +95,25 @@ export const xkcd: GlobalCommand = {
 		};
 
 		// let the users know that we are getting a post, and determine which comic to getto get
-		sendTyping;
+		sendTyping();
 		const latestComic = await _latestCheck(); // a  sanity check, getting the most recent comic to know the valid range of comics
 
+		if (latestComic.num === -1) {
+			await _sendErr(reply, 'XKCD call failed. Please try again later.');
+			return;
+		}
+
+		// determinining if a number was passed as an arg
 		const param = options[0];
 		if (param?.value !== undefined) {
 			// number was provided in the command
 			comic = `${param.value as number}`;
 			if (param.value < 1 || param.value > latestComic.num) {
-				// error checking, no negative comics or comic 0. Instead get the latest
-				await reply({
-					ephemeral: true,
-					content: `Please insert a valid comic number. The range is 1-${latestComic.num}.`,
-				});
+				// error checking, no negative comics or comic 0. Instead send an error message
+				await _sendErr(
+					reply,
+					`Please insert a valid comic number. The range is 1-${latestComic.num}.`
+				);
 				return;
 			}
 		} else {
@@ -77,14 +128,14 @@ export const xkcd: GlobalCommand = {
 				);
 				if (status !== 200) {
 					console.log('Axios failed to get a comic');
-					await reply({ content: 'XKCD call failed. Please try later.', ephemeral: true });
+					await _sendErr(reply, 'XKCD call returned an error. Please try again later.');
 					return;
 				}
 				results = data;
 			} catch (error) {
 				console.log('Axios failed to get due to exception');
 				console.log(error);
-				await reply({ content: 'XKCD call failed. Please try later.', ephemeral: true });
+				await _sendErr(reply, 'XKCD call failed. Please try again later.');
 				return;
 			}
 		} else {
