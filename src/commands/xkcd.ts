2e0e64eed49143c29fb7a1584da2109507ca0369
gmacgre <gmacgre@byu.edu>
2022-10-01 10:45:20 -0600

    Prepping for pulling changes from main

--- src/commands/xkcd.ts
+++ src/commands/xkcd.ts
@@ -1,4 +1,14 @@
-import { ApplicationCommandOptionType } from 'discord.js';
+import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import axios from 'axios';
+
+// defining the response
+interface GetComicResponse {
+	month: string;
+	num: number;
+	year: string;
+	title: string;
+	day: string;
+}
 
 export const xkcd: GlobalCommand = {
 	name: 'xkcd',
@@ -12,13 +22,41 @@ export const xkcd: GlobalCommand = {
 		},
 	],
 	async execute({ options, reply, sendTyping }) {
-		// TODO: finish this command
+		let comic: string = '';
+		let results: GetComicResponse = {
+			month: '',
+			num: 0,
+			year: '',
+			title: '',
+			day: '',
+		};
 		sendTyping;
 		const param = options[0];
 		if (param?.value !== undefined) {
-			await reply(`You sent a param! ${param.value as number}`);
+			comic = `${param.value as number}`;
+			// await reply(`You sent a param! ${param.value as number}`);
 		} else {
-			await reply('You did not send a param.');
+			comic = 'latest';
+			// await reply('You did not send a param.');
 		}
+		try {
+			const { data, status } = await axios.get<GetComicResponse>(
+				`https://xkcd.now.sh/?comic=${comic}`
+			);
+			if (status !== 200) {
+				console.log('Axios failed to get a comic');
+				await reply('Oopsie! I did a poopie.');
+				return;
+			}
+			results = data;
+		} catch {
+			console.log('Axios failed to get due to exception');
+			await reply('Oopsie! I did a poopie.');
+			return;
+		}
+
+		// we should have the data in response, build the embed.
+		// TODO: POST MERGE WITH MAIN, ADD THE FOOTER APPVERSION
+		const embed = new EmbedBuilder().setTitle(results.title).setColor(0x2b96f3);
 	},
 };
