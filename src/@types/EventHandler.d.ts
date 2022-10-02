1d8d249bbe2f75a5903bcec0b009a1385e660dab
Justin McBride <jstnmcbrd@outlook.com>
2022-10-02 03:47:28 -0600

    Event handler typing
    - created a TS type for Event Handlers, similar to Commands
    - created an ./events folder and an index file, similar to ./commands

---
+++ src/@types/EventHandler.d.ts
@@ -0,0 +1,25 @@
+// IDK why this line is necessary, but if I remove it, I can't use EventHandler anywhere
+import type { Awaitable } from 'discord.js';
+
+declare global {
+	/**
+	 * Defines an event handler.
+	 */
+	interface EventHandler {
+		/**
+		 * The name of the event this handler is for
+		 */
+		name: string;
+
+		/**
+		 * Whether this event can only fire once
+		 * Defaults to false
+		 */
+		once: false;
+
+		/**
+		 * The event implementation. Receives any number of arguments, depending on the event.
+		 */
+		execute: (...args: Array<any>) => Awaitable<void>;
+	}
+}
---
+++ src/events/index.ts
@@ -0,0 +1,50 @@
+export const allEventHandlers = new Map<string, EventHandler>();
+import type { Client } from 'discord.js';
+
+import { getLogger } from '../logger';
+const logger = getLogger();
+
+// Install event handlers
+// import { error } from './error';
+// _add(error);
+// import { interactionCreate } from './interactionCreate';
+// _add(interactionCreate);
+// import { ready } from './ready';
+// _add(ready);
+
+/**
+ * Adds an event handler to the list of all handlers.
+ * Does NOT register the event handler with the client. Use registerEventHandlers(Client) to update handlers.
+ * Only public for testing purposes. Avoid using.
+ * @param eventHandler The event handler to add
+ * @private
+ */
+export function _add(eventHandler: EventHandler): void {
+	const name: string = eventHandler.name;
+
+	if (allEventHandlers.has(name)) {
+		throw new TypeError(
+			`Failed to add event handler for '${name}' when a handler for that event was already added`
+		);
+	}
+
+	allEventHandlers.set(name, eventHandler);
+}
+
+/**
+ * Registers all event handlers with the client.
+ * @param client The client to register event handlers with
+ * @public
+ */
+export function registerEventHandlers(client: Client): void {
+	allEventHandlers.forEach((eventHandler: EventHandler, eventName: string) => {
+		// Register the event handler with the correct endpoint
+		if (eventHandler.once) {
+			client.once(eventName, eventHandler.execute);
+		} else {
+			client.on(eventName, eventHandler.execute);
+		}
+
+		logger.debug(`Registered event handler ${eventHandler.once ? 'once' : 'on'}(${eventName})`);
+	});
+}
