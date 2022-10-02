303fd3b097b8a9fe8a61763966c8e610de6ccf43
Justin McBride <jstnmcbrd@outlook.com>
2022-10-01 18:25:33 -0600

    Bad punctuation
    Co-authored-by: James Robinson <1500092+AverageHelper@users.noreply.github.com>

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,6 @@
 /**
- * A method that returns the default logger for this application
- * Uses a function instead of a constant to allow mocking
+ * A method that returns the default logger for this application.
+ * Uses a function instead of a constant to allow mocking.
  * @returns The default logger for this application
  */
 export function getLogger(): Console {
