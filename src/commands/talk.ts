e0e130adf8daebb7ffeb92bdd227772c8c783daa
Justin McBride <jstnmcbrd@outlook.com>
2022-11-28 22:46:22 -0700

    Cleanup from merge
    - removed unnecessary typecasting
    - standardized comments, because I'm picky like that

--- src/commands/talk.ts
+++ src/commands/talk.ts
@@ -6,7 +6,6 @@ import {
 	AttachmentPayload,
 	ChannelType,
 	PermissionFlagsBits,
-	CommandInteractionOptionResolver,
 } from 'discord.js';
 import {
 	createAudioResource,
@@ -49,9 +48,9 @@ export const talk: GlobalCommand = {
 	info: builder,
 	requiresGuild: false,
 	async execute(context) {
-		const options = context.interaction.options as CommandInteractionOptionResolver;
+		const options = context.interaction.options;
 
-		// this shouldn't happen, but it's good to have error checking anyway
+		// This shouldn't happen, but it's good to have error checking anyway
 		if (options.data.length === 0) {
 			throw new Error('No options provided');
 		}
@@ -75,28 +74,28 @@ export async function speak(
 	speakerNum: Speaker | undefined,
 	{ channel, client, prepareForLongRunningTasks, reply }: BaseCommandContext
 ): Promise<void> {
-	// this also shouldn't happen, but better safe than sorry
+	// This also shouldn't happen, but better safe than sorry
 	if (!channel) {
 		throw new Error('No channel');
 	}
 
-	// this could happen if the user uses the context menu command on an empty message
+	// This could happen if the user uses the context menu command on an empty message
 	if (!message || message === '') {
 		throw new Error('Message cannot be empty');
 	}
 
-	// generating the audio can take a while, so make sure Discord doesn't think we died
+	// Generating the audio can take a while, so make sure Discord doesn't think we died
 	await prepareForLongRunningTasks(false);
 
-	// generate the audio and store it in a generic buffer
+	// Generate the audio and store it in a generic buffer
 	log('generating audio');
 	const wavData: Buffer = await say(message, {
 		Speaker: speakerNum,
 	});
 
-	// if the command was given in a voice chat, speak it
+	// If the command was given in a voice chat, speak it
 	if (channel.type === ChannelType.GuildVoice) {
-		// make sure we have the right permissions to talk
+		// Make sure we have the right permissions to talk
 		const permissions = channel.permissionsFor(client.user);
 		if (!permissions) {
 			throw new Error('Could not fetch permissions for channel');
@@ -108,26 +107,26 @@ export async function speak(
 			throw new Error('Missing required permissions to speak in voice channel');
 		}
 
-		// check to see if we're already connected to this channel
-		// a channel can only have one connection & one player at a time!
+		// Check to see if we're already connected to this channel
+		// A channel can only have one connection & one player at a time!
 		if (getVoiceConnection(channel.guild.id)) {
 			throw new Error('Already talking in channel\n\nPlease try again later');
 		}
 
-		// store the audio buffer in a temporary .wav file to pass to discordjs/voice
+		// Store the audio buffer in a temporary .wav file to pass to discordjs/voice
 		const tempFile = fileSync({ prefix: 'dectalk', postfix: '.wav' });
 		writeFileSync(tempFile.name, wavData);
 		const stream = createReadStream(tempFile.name);
 
-		// create an audio resource from the temporary file
+		// Create an audio resource from the temporary file
 		let resource: AudioResource;
 		try {
 			resource = createAudioResource(stream);
 		} catch (error) {
-			// if it's not an error, leave it alone
+			// If it's not an error, leave it alone
 			if (!(error instanceof Error)) throw error;
 
-			// for this error, provide a more useful message
+			// For this error, provide a more useful message
 			if (error.message === 'FFmpeg/avconv not found!') {
 				throw new Error(
 					"'ffmpeg-static' missing proper encoder for current OS\n\n" +
@@ -135,22 +134,22 @@ export async function speak(
 				);
 			}
 
-			// remember to close file stream
+			// Remember to close file stream
 			stream.close();
 
-			// don't tamper with any other errors
+			// Don't tamper with any other errors
 			throw error;
 		}
 
-		// create a player to stream the resource
+		// Create a player to stream the resource
 		const player = createAudioPlayer({
 			behaviors: {
-				// play even when there's no one in the channel
+				// Play even when there's no one in the channel
 				noSubscriber: NoSubscriberBehavior.Play,
 			},
 		});
 
-		// create a new audio connection to the voice channel the command was given in
+		// Create a new audio connection to the voice channel the command was given in
 		log('joining channel');
 		const connection = joinVoiceChannel({
 			channelId: channel.id,
@@ -159,14 +158,14 @@ export async function speak(
 			selfMute: false,
 		});
 
-		// when the connection is ready, attach the player and give the player the audio
+		// When the connection is ready, attach the player and give the player the audio
 		connection.once(VoiceConnectionStatus.Ready, () => {
 			log('connection ready');
 			connection.subscribe(player);
 			player.play(resource);
 		});
 
-		// when the player finishes playing the audio, disconnect if connected, and delete the audio file
+		// When the player finishes playing the audio, disconnect if connected, and delete the audio file
 		player.on(AudioPlayerStatus.Idle, () => {
 			log('player done');
 
@@ -177,11 +176,11 @@ export async function speak(
 				connection.disconnect();
 			}
 
-			// remember to close file stream
+			// Remember to close file stream
 			stream.close();
 		});
 
-		// when the connection closes, stop the player if playing and end the interaction
+		// When the connection closes, stop the player if playing and end the interaction
 		connection.on(VoiceConnectionStatus.Disconnected, () => {
 			log('disconnected');
 			connection.destroy();
@@ -194,7 +193,7 @@ export async function speak(
 			player.stop();
 		});
 
-		// if the connection doesn't succeed within 10 seconds, clean up and throw an error to end the interaction
+		// If the connection doesn't succeed within 10 seconds, clean up and throw an error to end the interaction
 		try {
 			await entersState(connection, VoiceConnectionStatus.Ready, 10e3);
 		} catch {
@@ -204,7 +203,7 @@ export async function speak(
 			throw new Error('Could not connect to voice channel');
 		}
 	} else {
-		// if the command was given in a text channel, send it as an attachment
+		// If the command was given in a text channel, send it as an attachment
 		log('sending attachment audio');
 
 		const attachment: AttachmentPayload = {
