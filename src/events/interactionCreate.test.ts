cc1d323776f5fa2570098aca832c84de6281eaed
Koby Lewis <kobyjlewis@gmail.com>
2022-12-27 16:47:43 -0700

    add userMessageError test

--- src/events/interactionCreate.test.ts
+++ src/events/interactionCreate.test.ts
@@ -19,6 +19,7 @@ import {
 	ContextMenuCommandBuilder,
 	SlashCommandBuilder,
 } from 'discord.js';
+import { UserMessageError } from '../helpers/UserMessageException';
 
 // Mock allCommands to isolate our test code
 const mockAllCommands = new Map<string, Command>();
@@ -126,6 +127,18 @@ const mockErrorGuildedCommand: Command = {
 	},
 };
 mockAllCommands.set(mockErrorGuildedCommand.info.name, mockErrorGuildedCommand);
+
+const userErrorMessage = 'This is a user message error message';
+const mockUserMessageErrorGlobalCommand: Command = {
+	info: new SlashCommandBuilder() //
+		.setName('global-error-test')
+		.setDescription('whoops'),
+	requiresGuild: false,
+	execute: () => {
+		throw new UserMessageError(userErrorMessage);
+	},
+};
+mockAllCommands.set(mockUserMessageErrorGlobalCommand.info.name, mockUserMessageErrorGlobalCommand);
 
 // Mock allButtons to isolate our test code
 const mockAllButtons = new Map<string, Button>();
@@ -327,6 +340,23 @@ describe('on(interactionCreate)', () => {
 
 			await expect(interactionCreate.execute(interaction)).resolves.toBeUndefined();
 			expect(mockInteractionReply).toHaveBeenCalledOnce();
+		});
+
+		test('sends an error embed with a specified message when global command throws an error', async () => {
+			const interaction = defaultInteraction();
+			(interaction as CommandInteraction).commandName = mockUserMessageErrorGlobalCommand.info.name;
+
+			const mockInteractionReply = jest.fn();
+			(interaction as CommandInteraction).reply = mockInteractionReply;
+
+			await expect(interactionCreate.execute(interaction)).resolves.toBeUndefined();
+			expect(mockInteractionReply).toHaveBeenCalledOnce();
+
+			const lastCall = mockInteractionReply.mock.lastCall as unknown as [
+				{ embeds: [{ data: { description: string } }] }
+			];
+			const description = lastCall[0].embeds[0].data.description;
+			expect(description).toBe(userErrorMessage);
 		});
 
 		test('sends an error embed message when message context menu command throws an error', async () => {
