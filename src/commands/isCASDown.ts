6a518580d283d67598d570800658e3489b801e70
Joseph Elijah Geis <joseph@josephgeis.dev>
2023-03-16 00:35:52 -0600

    Add isCASDown command

---
+++ src/commands/isCASDown.ts
@@ -0,0 +1,42 @@
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import axios from 'axios';
+
+const statusURI = 'https://cas.byu.edu/cas/serviceValidate';
+
+const statuses = {
+	down: {
+		message: 'CAS is down again.',
+		color: 0xdc2626,
+	},
+	up: { message: 'CAS is running, for now.', color: 0x16a34a },
+};
+
+const builder = new SlashCommandBuilder()
+	.setName('iscasdown')
+	.setDescription('Checks if CAS is down');
+
+export const isCASDown: GlobalCommand = {
+	info: builder,
+	requiresGuild: false,
+	async execute({ reply }) {
+		const embed = new EmbedBuilder().setTitle('Is CAS Down?');
+
+		let status = statuses.down;
+
+		const req = await axios.get(statusURI).catch(() => null);
+
+		if (req) {
+			if (req.status == 200) {
+				status = statuses.up;
+			}
+		}
+
+		embed.setColor(status.color);
+		embed.setDescription(status.message);
+
+		await reply({
+			embeds: [embed],
+			ephemeral: true,
+		});
+	},
+};
