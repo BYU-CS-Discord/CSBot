d1789063c4358ea0cda23fdabfadab2893dee15e
Justin McBride <jstnmcbrd@outlook.com>
2023-03-18 02:40:02 -0600

    Fix tests

--- src/commands/contextMenu/fxtwitter.test.ts
+++ src/commands/contextMenu/fxtwitter.test.ts
@@ -1,7 +1,7 @@
 // Mock findTwitterURLs
-const mockFindTwitterURLs = jest.fn();
-jest.mock('../../helpers/findTwitterURLs', () => ({
-	findTwitterURLs: mockFindTwitterURLs,
+const mockFindTwitterUrls = jest.fn();
+jest.mock('../../helpers/findTwitterUrls', () => ({
+	findTwitterUrls: mockFindTwitterUrls,
 }));
 
 import { fxtwitter } from './fxtwitter';
@@ -17,11 +17,11 @@ describe('Fix Twitter Links', () => {
 			},
 			replyPrivately: mockReplyPrivately,
 		} as unknown as MessageContextMenuCommandContext;
-		mockFindTwitterURLs.mockReturnValue([]);
+		mockFindTwitterUrls.mockReturnValue([]);
 	});
 
 	test('complains at the caller if there are no proper Twitter links', async () => {
-		mockFindTwitterURLs.mockReturnValue([]);
+		mockFindTwitterUrls.mockReturnValue([]);
 		await expect(fxtwitter.execute(context)).rejects.toThrow();
 	});
 
@@ -31,9 +31,9 @@ describe('Fix Twitter Links', () => {
 		${[new URL('https://twitter.com/example2')]}                                         | ${'https://fxtwitter.com/example2'}
 		${[new URL('https://twitter.com/example'), new URL('https://twitter.com/example2')]} | ${'https://fxtwitter.com/example\nhttps://fxtwitter.com/example2'}
 	`(
-		'sends an ephemeral message containing fixed versions of the Twitter links returned by findTwitterURLs',
+		'sends an ephemeral message containing fixed versions of the Twitter links returned by findTwitterUrls',
 		async ({ urls, result }: { urls: string; result: string }) => {
-			mockFindTwitterURLs.mockReturnValue(urls);
+			mockFindTwitterUrls.mockReturnValue(urls);
 
 			await expect(fxtwitter.execute(context)).resolves.toBeUndefined();
 			expect(mockReplyPrivately).toHaveBeenCalledOnce();
