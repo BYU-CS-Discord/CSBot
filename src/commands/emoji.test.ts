7fc0606416aa2ea480663fd9febcaf0d8162b524
Thomas Stansel <thomas.s.stansel@gmail.com>
2023-03-30 23:43:52 +0000

    Add respondEphemeral option to slash command

--- src/commands/emoji.test.ts
+++ src/commands/emoji.test.ts
@@ -7,6 +7,7 @@ describe('profile', () => {
 	const mockReply = jest.fn<Promise<void>, [content: unknown]>();
 	const mockEmojiName = jest.fn<string | null, []>();
 	const mockEmojiURL = jest.fn<string | null, [options?: ImageURLOptions | undefined]>();
+	const mockRespondEphemeral = jest.fn<boolean | null, []>();
 
 	const testEmojiURL = 'https://example.com/emojis/1234567890/abcdef1234567890.png';
 
@@ -28,6 +29,7 @@ describe('profile', () => {
 						},
 					},
 					getString: mockEmojiName,
+					getBoolean: mockRespondEphemeral,
 				},
 			},
 			reply: mockReply,
@@ -35,11 +37,7 @@ describe('profile', () => {
 
 		mockEmojiName.mockReturnValue('existing emoji');
 		mockEmojiURL.mockReturnValue(testEmojiURL);
-	});
-
-	test('Throws an error when no input is provided', async () => {
-		mockEmojiName.mockReturnValue(null);
-		await expect(emoji.execute(context)).rejects.toThrow('You must provide an emoji to search for');
+		mockRespondEphemeral.mockReturnValue(true);
 	});
 
 	test('Throws an error when the target emoji does not exist', async () => {
@@ -47,7 +45,7 @@ describe('profile', () => {
 		await expect(emoji.execute(context)).rejects.toThrow('Emoji emoji not here was not found');
 	});
 
-	test('Returns the url of the target emoji', async () => {
+	test('Returns the url of the target emoji ephemerally', async () => {
 		await expect(emoji.execute(context)).resolves.toBeUndefined();
 
 		expect(mockReply).toHaveBeenCalledOnce();
@@ -59,7 +57,41 @@ describe('profile', () => {
 					image: { url: testEmojiURL },
 				}),
 			],
-			ephemeral: true,
+			ephemeral: mockRespondEphemeral() as boolean,
+		});
+	});
+
+	test('Returns the url of the target emoji ephemerally with undefined respondEphemeral', async () => {
+		mockRespondEphemeral.mockReturnValue(null);
+		await expect(emoji.execute(context)).resolves.toBeUndefined();
+
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			content: mockEmojiName() as string,
+			embeds: [
+				new EmbedBuilder({
+					title: mockEmojiName() as string,
+					image: { url: testEmojiURL },
+				}),
+			],
+			ephemeral: true, // slash command defaults to true when no respondEphemeral boolean is given
+		});
+	});
+
+	test('Returns the url of the target emoji non-ephemerally', async () => {
+		mockRespondEphemeral.mockReturnValue(false);
+		await expect(emoji.execute(context)).resolves.toBeUndefined();
+
+		expect(mockReply).toHaveBeenCalledOnce();
+		expect(mockReply).toHaveBeenCalledWith({
+			content: mockEmojiName() as string,
+			embeds: [
+				new EmbedBuilder({
+					title: mockEmojiName() as string,
+					image: { url: testEmojiURL },
+				}),
+			],
+			ephemeral: mockRespondEphemeral() as boolean,
 		});
 	});
 });
--- src/commands/emoji.ts
+++ src/commands/emoji.ts
@@ -2,22 +2,31 @@ import { DiscordAPIError, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import * as logger from '../logger';
 
 const EmojiName = 'emojiname';
+const RespondEphemeral = 'respondephemeral';
 
 const builder = new SlashCommandBuilder()
 	.setName('emoji')
 	.setDescription('Responds with the picture used for the mentioned emoji')
 	.addStringOption(option =>
 		option.setName(EmojiName).setDescription('Name of the emoji to search for').setRequired(true)
+	)
+	.addBooleanOption(option =>
+		option
+			.setName(RespondEphemeral)
+			.setDescription(
+				'When set to false this will respond with the emoji image so everyone can see. Default is true'
+			)
 	);
 
 export const emoji: GlobalCommand = {
 	info: builder,
 	requiresGuild: false,
 	async execute({ reply, interaction }): Promise<void> {
-		const emojiName = interaction.options.getString(EmojiName);
-		if (emojiName === null) {
-			throw new Error('You must provide an emoji to search for');
-		}
+		const emojiName = interaction.options.getString(EmojiName, true);
+		const respondEphemeral =
+			interaction.options.getBoolean(RespondEphemeral) !== null
+				? interaction.options.getBoolean(RespondEphemeral, true)
+				: true; // This slash command defaults to sending an ephemeral message
 
 		const emojiCache = interaction.options.client.emojis.cache;
 		const foundEmoji = emojiCache.find(emojiElement => emojiElement.name === emojiName);
@@ -29,6 +38,6 @@ export const emoji: GlobalCommand = {
 		const url = foundEmoji.url;
 		const embed = new EmbedBuilder().setTitle(emojiName).setImage(url);
 
-		await reply({ content, embeds: [embed], ephemeral: true });
+		await reply({ content, embeds: [embed], ephemeral: respondEphemeral });
 	},
 };
