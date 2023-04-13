6a17ecdeda2d86706e1de8ae91849c3cac7de62c
Koby Lewis <kobyjlewis@gmail.com>
2023-04-13 16:10:25 +0000

    partial reactboard message finding

--- src/reactionHandlers/updateReactboard.ts
+++ src/reactionHandlers/updateReactboard.ts
@@ -1,10 +1,36 @@
+import { ChannelType } from 'discord.js';
 import { db } from '../database';
 
 export const updateReactboard: ReactionHandler = {
-	async execute({ reaction }) {
+	async execute({ reaction, user }) {
 		if (reaction.message.guildId === null) return; // ignore guildless messages? How is that a thing?
 		if (reaction.emoji.id === null) return; // idless reactions???
 		const fullReaction = await reaction.fetch();
+
+		const reactboardPosts = await db.reactboardPost.findMany({
+			where: {
+				originalMessageId: reaction.message.id,
+			},
+			include: {
+				reactboard: true,
+			},
+		});
+
+		if (reactboardPost.length > 0) {
+			const updatePromises = reactboardPosts.map(async reactboardPost => {
+				const reactboardChannel = await reaction.client.channels.cache
+					.get(reactboardPost.reactboard.channelId)
+					?.fetch();
+				if (
+					reactboardChannel === undefined ||
+					!reactboardChannel.isTextBased() ||
+					reactboardChannel.type === ChannelType.GuildStageVoice
+				) {
+					throw new Error('Could not find channel');
+				}
+				reactboardChannel.messages
+			})
+		}
 
 		const reactboards = await db.reactboard.findMany({
 			where: {
