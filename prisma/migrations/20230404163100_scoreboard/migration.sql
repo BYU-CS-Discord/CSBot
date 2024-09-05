4bc979f87ffd1ff7600fdc5a2cfe2308cab5844c
AverageHelper <me@average.name>
2024-09-05 10:45:00 -0600

    chore: Remove incompatible PostgreSQL migrations

--- prisma/migrations/20230404163100_scoreboard/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Scoreboard" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "score" DOUBLE PRECISION NOT NULL,
-
-    CONSTRAINT "Scoreboard_pkey" PRIMARY KEY ("id")
-);
