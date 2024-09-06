1585137b69fe0946d7dcb78a3f9946deae5d701f
AverageHelper <me@average.name>
2024-09-06 13:51:46 -0600

    chore: Don't migrate twice

--- scripts/launch_in_docker.sh
+++ scripts/launch_in_docker.sh
@@ -3,7 +3,6 @@ DATABASE_FILENAME="$(echo $DATABASE_URL | sed "s/file://g")"
 if [[ ! -f $DATABASE_FILENAME ]] ; then
 	echo "No database detected, initializing..."
 	npm run db:init
-	npm run db:migrate
 fi
 node . --deploy
 node .
