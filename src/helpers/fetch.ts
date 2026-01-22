f86f38fccacd97c93f7b221718aef04899aa6474
AverageHelper <me@average.name>
2026-01-22 09:02:14 -0700

    fix: this one was just a `type` import lol

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,4 +1,4 @@
-import { URL } from 'node:url';
+import type { URL } from 'node:url';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { Struct, StructError } from 'superstruct';
 import { assert } from 'superstruct';
